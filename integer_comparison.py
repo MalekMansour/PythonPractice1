1a9dab52232b197336c8c986dc682c5406e49f8e
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2023-12-26 00:39:26 -0700

    Create integer_comparison.py

---
+++ integer_comparison.py
@@ -0,0 +1,10 @@
+# Integer Comparison
+user_choice1 = int(input("Enter the first integer: "))
+user_choice2 = int(input("Enter the second integer: "))
+
+if user_choice1 == user_choice2:
+    print("These numbers are equal.")
+if user_choice1 > user_choice2:
+    print(user_choice1,"is larger.")
+if user_choice2 > user_choice1:
+    print(user_choice2,"is larger.")
