fb5ea6c1740654f4e7943cc2eab9c8e594fc3499
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2023-12-25 19:54:26 -0700

    Create area_trapezoid.py

---
+++ area_trapezoid.py
@@ -0,0 +1,8 @@
+# Area of trapezoid
+height = int(input("Height: "))
+base_a = int(input("Base A: "))
+base_b = int(input("Base B: "))
+bases = base_a + base_b
+x = height * bases
+area = x / 2
+print("Area of the Trapezoid is", area)
