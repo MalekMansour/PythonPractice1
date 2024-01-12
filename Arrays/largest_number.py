fa0880dbdc54324260c93b9caa9af04a8e35717a
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-01-12 01:23:49 -0700

    Create largest_number.py

---
+++ Arrays/largest_number.py
@@ -0,0 +1,8 @@
+def find_max_element(arr):
+    largest_number = max(arr)
+    return largest_number
+
+# Test cases
+print(find_max_element([3, 7, 1, 9, 5]))  # Output: 9
+print(find_max_element([-1, -5, -2, -8]))  # Output: -1
+print(find_max_element([0, 2, 4, 6, 8]))   # Output: 8
