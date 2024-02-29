46dcc88271dc781106fadb51e12bcf6a5da1600f
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-02-29 07:52:49 -0700

    Update Device_Manager.py

--- practice2/Device_Manager.py
+++ practice2/Device_Manager.py
@@ -15,17 +15,21 @@ class System:
 
     def display_devices(self):
         print("Devices in the system:")
-        for device in self.devices:
-            print("-", device)
+        if self.devices:
+            for idx, device in enumerate(self.devices, start=1):
+                print(f"{idx}. {device}")
+        else:
+            print("No devices found in the system.")
 
 
 def main():
     system = System()
     
     while True:
-        print("\n1. Add device")
-        print("2. Remove device")
-        print("3. Display devices")
+        print("\nChoose an option:")
+        print("1. Add a device")
+        print("2. Remove a device")
+        print("3. Display all devices")
         print("4. Exit")
         
         choice = input("Enter your choice (1/2/3/4): ")
@@ -34,8 +38,14 @@ def main():
             device = input("Enter the name of the device to add: ")
             system.add_device(device)
         elif choice == '2':
-            device = input("Enter the name of the device to remove: ")
-            system.remove_device(device)
+            system.display_devices()
+            if system.devices:
+                device_idx = int(input("Enter the index of the device to remove: "))
+                if 1 <= device_idx <= len(system.devices):
+                    device = system.devices[device_idx - 1]
+                    system.remove_device(device)
+                else:
+                    print("Invalid device index.")
         elif choice == '3':
             system.display_devices()
         elif choice == '4':
