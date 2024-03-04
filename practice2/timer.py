3687fc900d49495b01f2aa8217ec2ef1c3af8faa
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-04 08:44:49 -0700

    Update timer.py

--- practice2/timer.py
+++ practice2/timer.py
@@ -1,1 +1,16 @@
+import time
+# timer
+def countdown(seconds):
+  
+  while seconds:
+    hours = seconds // 3600
+    minutes = (seconds % 3600 ) // 60
+    secs = seconds % 60
+    
+    timer = '{:02d}:{:02d}:{:02d}'.format(hours, minutes, secs)
+    print(timer, end="\r")
+    time.sleep(1)
+    seconds -= 1
+seconds = int(input("Enter the time in seconds: "))
 
+countdown(seconds)
