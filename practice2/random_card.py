3e511ca6f791fa3e8b1f162f438013332b26ba70
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-04 08:45:46 -0700

    Update random_card.py

--- practice2/random_card.py
+++ practice2/random_card.py
@@ -1,1 +1,8 @@
-
+import random
+# suits and ranks
+suits = ("♡", "♤", "♢", "♧")
+ranks = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+# choose a random card and shows it
+Family = random.choice(suits)
+No = random.choice(ranks)
+print(No, Family)
