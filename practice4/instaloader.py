6a7da89f9962ccfdd4fbed24c3b69ac562afab5a
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-08-10 18:49:30 +0100

    .

--- practice4/instaloader.py
+++ practice4/instaloader.py
@@ -1,2 +1,13 @@
 import instaloader
 
+bot = instaloader.Instaloader()
+
+username = input("Enter the user name: ")
+profile = instaloader.Profile.from_username(bot.context, username)
+
+print("Username: ", profile.username)
+print("User ID: ", profile.userid)
+print("Number of Posts: ", profile.mediacount)
+print("Followers: ", profile.followers)
+print("Followees: ", profile.followees)
+print("Bio: ", profile.biography, profile.external_url)
